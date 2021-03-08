cac3c44ef997f09d50822c04e5ef7865ae36a337
Roman Dubchak <dubrathers@mail.ru>
2021-03-08 14:35:00 +0300

    create DeliveryAddressInput

---
+++ src/main/java/com/geekbrains/geekspring/dto/DeliveryAddressInput.java
@@ -0,0 +1,20 @@
+package com.geekbrains.geekspring.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class DeliveryAddressInput {
+    @NotBlank
+    private String deliveryAddress;
+
+    @NotBlank
+    @Size(min = 15, max = 15)
+    @Pattern(regexp = "([0,9]{1,})", message ="phone number can contain numbers from 0 to 9")
+//    @Pattern(regexp = "^(\\\\+\\\\d{1,3}( )?)?((\\\\(\\\\d{3}\\\\))|\\\\d{3})[- .]?\\\\d{3}[- .]?\\\\d{4}$",
+//            message ="phone number can contain numbers from 0 to 9")
+    private String phoneForDelivery;
+}
